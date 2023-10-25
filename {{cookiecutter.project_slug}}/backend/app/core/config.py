6f6350a0eb99b67b06f6902e8d4eb9449b726350
InSeong.Lee <randy80@gmail.com>
2023-10-25 17:22:17 +0900

    fix: [Backend] Fix to prevent errors related to secrets_dir

--- {{cookiecutter.project_slug}}/backend/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/core/config.py
@@ -7,7 +7,7 @@ from pydantic_settings_yaml import YamlBaseSettings
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class Settings(YamlBaseSettings):
-    model_config = SettingsConfigDict(yaml_file=f'{ROOT_DIR}/config/config.yaml', env_file_encoding='utf-8', secrets_dir=None)
+    model_config = SettingsConfigDict(yaml_file=f'{ROOT_DIR}/config/config.yaml', env_file_encoding='utf-8')
 
     domain: str
 
